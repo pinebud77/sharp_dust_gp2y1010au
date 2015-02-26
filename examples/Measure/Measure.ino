e012c30bf21402a2bf37aecca597757e2b19a1e9
Owen Kwon <ohkeunow@qti.qualcomm.com>
2015-02-26 17:09:15 +0900

    added example

---
+++ examples/Measure/Measure.ino
@@ -0,0 +1,19 @@
+#include <SharpDust.h>
+
+#define DUST_LED_PIN		2
+#define DUST_MEASURE_PIN	0
+
+void setup() {
+  Serial.begin(9600);
+
+  SharpDust.begin(DUST_LED_PIN, DUST_MEASURE_PIN);
+}
+
+void loop() {
+  while (1) {
+    Serial.print(F("Dust :"));
+    Serial.print(SharpDust.measure());
+    Serial.println(F(" mg/m^3"));
+    delay(1000);
+  }
+}
